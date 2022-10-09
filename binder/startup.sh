c8bba8767fee83d6eb3d59832c014bb3ef7ace77
Andrew Bauman <baumanab@gmail.com>
2022-10-08 21:55:53 -0700

    Update startup.sh

--- binder/startup.sh
+++ binder/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PYSPARK_DRIVER_PYTHON=jupyter
-export PYSPARK_DRIVER_PYTHON_OPTS='lab --ip=0.0.0.0'
+export PYSPARK_DRIVER_PYTHON_OPTS='lab --port 8889'
 
 exec $SPARK_HOME/bin/pyspark --packages io.delta:${DELTA_PACKAGE_VERSION} \
 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
